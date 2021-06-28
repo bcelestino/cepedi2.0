8e2af2484f847f2c66969068c0ac9df96da35522
Bruna Celestino <celestino.b@hotmail.com>
2021-06-28 20:05:48 -0300

    first commit

---
+++ src/test/java/com/cepedi/cepedi20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepedi.cepedi20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
